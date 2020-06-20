6042e7d440864635ba823893b70307d21e9a1d1e
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-06-20 11:58:20 -0500

    Created Authoriation w/firebase

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,11 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import utils from '../../helpers/utils/utils';
 
 const signMeIn = () => {
-  console.warn('yeah sure doc');
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 };
 
 const loginButton = () => {
-
   const domString = '<button id="google-auth" class="btn btn-warning"><i class="fab fa-google-plus"></i> LOG ME IN!!!</button>';
   utils.printToDom('#auth', domString);
   $('#google-auth').click(signMeIn);
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,16 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
 import 'bootstrap';
 import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+import myNavBar from './components/auth/myNavBar/myNavBar';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLogInStatus();
   auth.loginButton();
-  console.error('HEY MY DUDE');
+  myNavBar.logoutEvent();
 };
 
 init();
