c50985626c8898d2b051576c803c0ca068fb7cfb
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-06-20 13:52:20 -0500

    WIP

---
+++ src/javascripts/components/mushroomList/mushshroom.js
@@ -0,0 +1,12 @@
+// import utils from '../../helpers/utils/utils';
+import mushroomData from '../../helpers/data/mushroomData';
+
+const buildForest = () => {
+  mushroomData.getMushrooms()
+    .then((response) => console.warn('get works', response.data))
+    .catch((err) => console.error('bad stuff', err));
+  // const domString = 'I see it!';
+  // utils.printToDom('#forest', domString);
+};
+
+export default { buildForest };
