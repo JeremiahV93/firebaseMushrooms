706d040603370c0e86a373cd0ec1bb4e1d2cb9c4
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-06-22 19:38:14 -0500

    Pulled data from Firebase API and created cards with new array

--- src/javascripts/components/mushroomList/mushshroom.js
+++ src/javascripts/components/mushroomList/mushshroom.js
@@ -1,9 +1,21 @@
-import utils from '../../helpers/utils/utils';
 import mushroomData from '../../helpers/data/mushroomData';
+import utils from '../../helpers/utils/utils';
+import mushroomComponent from '../mushroom/mushroom';
 
 const buildForest = () => {
   mushroomData.getMushrooms()
-    .then((response) => console.error('get works', response.data))
+    .then((mushrooms) => {
+      console.error('get works', mushrooms);
+      let domString = `
+      <h2 class="text-center">Forest</h2>
+      <div class="d-flex flex=wrap">
+      `;
+      mushrooms.forEach((mushroom) => {
+        domString += mushroomComponent.mushroomMaker(mushroom);
+      });
+      domString += '</div>';
+      utils.printToDom('#forest', domString);
+    })
     .catch((err) => console.error('bad stuff', err));
   const domString = 'I see it!';
   utils.printToDom('#forest', domString);
